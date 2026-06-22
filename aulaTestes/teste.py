c063df84a6b7ad12a6c550e6281124b89e6ce3f2
HMendesMartins <henriquemendesmartins9@gmail.com>
2026-06-22 09:40:41 -0300

    Create teste.py

---
+++ aulaTestes/teste.py
@@ -0,0 +1,7 @@
+def eh_par(numero):
+    return numero % 2 == 0
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
+def test_numero_par():
+    assert eh_par(4) == True
+test_numero_par()
+eh_par(4)
